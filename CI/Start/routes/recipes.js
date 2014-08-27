8a15668e3ef26856be1fbe4d2ed8920271c505cc
sedouard <steven.edouard1@gmail.com>
2014-08-27 16:29:49 -0700

    Add Start folder and the .gitignores for node.js

---
+++ CI/Start/routes/recipes.js
@@ -0,0 +1,11 @@
+﻿var recipes = require('../data/recipesData.js');
+
+exports.list = function (req, res) {
+    //get the name of the kind of recipes that was requested
+    var kind = req.params.id;
+
+    res.render('recipes', {
+        recipes: recipes.getRecipes(kind)
+    });
+
+}
